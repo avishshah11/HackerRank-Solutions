77cc09a8b3a5174fb40aa0db710359818e17433c
Avish Shah <avishshah1108@gmail.com>
2020-05-31 01:52:34 +0530

    Add files via upload

---
+++ python/Introduction_to_sets.py
@@ -0,0 +1,7 @@
+def average(arr):
+	return sum(set(arr))/ len(set(arr))
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
